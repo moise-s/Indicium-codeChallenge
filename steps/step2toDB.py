9278e25277bf8dfbbb88b33fdf936721b92c7993
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-08-10 19:10:17 -0300

    include conn

--- steps/step2toDB.py
+++ steps/step2toDB.py
@@ -1,18 +1,12 @@
 import pandas as pd
 import os
 from sqlalchemy import create_engine
+from conn import conn
 
 
 def localToDB():
 
-    user = 'user'
-    password = 'password'
-    host = 'localhost:3333'
-    dbname = 'northwind_complete'
-    dbengine = "mysql+pymysql://{}:{}@{}/{}".format(
-        user, password, host, dbname)
-    engine = create_engine(dbengine)
-
+    engine = conn.connMySql()
     rootDirPos = r"./data/POSTGRES/"
     for dirName, subdirList, fileList in os.walk(rootDirPos, topdown=False):
         for fname in fileList:
